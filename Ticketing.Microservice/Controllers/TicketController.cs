68d4fa75e244f1d7546cf4ac190494cda0a5100b
Nagaraj Natarajan <nagaraj.natarajan@boeing.com>
2022-05-07 11:21:44 +0530

    RabittMQ_MicroserviceSample

---
+++ Ticketing.Microservice/Controllers/TicketController.cs
@@ -0,0 +1,48 @@
+﻿using MassTransit;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Shared.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ticketing.Microservice.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TicketController : ControllerBase
+    {
+        private readonly ILogger<TicketController> _logger;
+        private readonly IBus _bus;
+        public TicketController(ILogger<TicketController> logger,IBus bus)
+        {
+            _bus = bus;
+            _logger = logger;
+        }
+
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+
+
+        [HttpPost]
+        [Route("createticket")]
+        public async Task<IActionResult> CreateTicket([FromBody]Ticket ticket)
+        {
+            if (ticket != null)
+            {
+                ticket.BookedOn = DateTime.Now;
+                Uri uri = new Uri("rabbitmq://localhost/ticketQueue");
+                var endPoint = await _bus.GetSendEndpoint(uri);
+                await endPoint.Send(ticket);
+                return Ok();
+            }
+            return BadRequest();
+        }
+    }
+}
